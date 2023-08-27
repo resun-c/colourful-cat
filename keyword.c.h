525880c54dd80c872511a624ef6e991cca11f9b7
Saif Resun <resun.saif@gmail.com>
2023-08-27 22:21:19 +0600

    Update keyword.c.h

--- keyword.c.h
+++ keyword.c.h
@@ -1,5 +1,8 @@
+#ifndef _STRING_H
 #include <string.h>
+#endif
 
+/* C keyword list */
 char *C_KW_LIST[14] = {
     "break",
     "else",
@@ -17,6 +20,7 @@ char *C_KW_LIST[14] = {
     "while"
 };
 
+/* C type list */
 char *C_TYPES_LIST[19] = {
     "auto",
     "char",
@@ -42,6 +46,7 @@ char *C_TYPES_LIST[19] = {
 int C_KW_LIST_LEN = 13;
 int C_TYPES_LIST_LEN = 18;
 
+/* check if keyw is a C keyword or not */
 int isckeyword (char *keyw)
 {
     int i;
@@ -51,6 +56,7 @@ int isckeyword (char *keyw)
     return 0;
 }
 
+/* check if ctyp is a C type or not */
 int isctype (char *ctyp)
 {
     int i;
